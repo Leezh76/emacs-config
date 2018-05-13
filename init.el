8cda5bcef23da963d599a1c9ad06d8be34028226
leezh76 <lizihong@outlook.com>
2018-05-13 16:45:52 +0800

    add init-packages.el

--- init.el
+++ init.el
@@ -1,95 +1,42 @@
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  ;;(setq package-archives '(("gnu" . "https://elpa.emacs-china.org/gnu/")
-  ;;			   ("melpa" . "https://elpa.emacs-china.org/melpa/"))))
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/")))
-(require 'cl)
-;;add package here
-(defvar my/pkgs '(
-		 company
-		 solarized-theme
-		 hungry-delete
-		 smex
-		 smartparens
-		 js2-mode
-		 nodejs-repl
-		 exec-path-from-shell
-		 window-numbering
-		 paredit
-		 ) "Default packages")
-(setq packages-selected-packages 'my/pkgs)
-(defun my/pkg-installed-p()
-  (loop for pkg in my/pkgs
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
-(unless (my/pkg-installed-p)
-  (message "%s" "Refreshing packages database...")
-  (package-refresh-contents)
-  (dolist (pkg my/pkgs)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
+(package-initialize)
 
-;;enable emacs to exec the shell commands
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-packages)
 
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(global-linum-mode 1)
-(global-auto-revert-mode t)
+(setq ring-bell-function 'ignore)
 
-(load-theme 'solarized-light t)
+
+(define-abbrev-table 'global-abbrev-table '(;;signature
+					    ("lee" "leezh")
+					    ;;emacs regex
+					    ))
 
 (setq cursor-type 'bar)
 (setq inhibit-splash-screen 1)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-(delete-selection-mode t)
-(global-hl-line-mode t)
+
 (set-face-attribute 'default nil :height 140)
 (defun open-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f2>") 'open-init-file)
 
-(require 'hungry-delete)
-(global-hungry-delete-mode t)
-
-(require 'smex)
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-
-(require 'smartparens-config)
-;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-
-(require 'window-numbering)
-(window-numbering-mode t)
-
-;;config js2-mode for js files
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       auto-mode-alist))
-
-(require 'org)
-(setq org-src-fontify-natively t)
-
 (require 'recentf)
 (recentf-mode 1)
 
-;;;my settings
-(require 'paredit)
-(add-hook 'scheme-mode-hook (lambda ()(paredit-mode 1)))
-;;my settings
+(require 'org)
+(setq org-src-fontify-natively t)
 
 ;;specfic kbd
 (global-set-key "\C-x\ \C-r" 'recentf-open-files) 
@@ -99,8 +46,13 @@
 
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode) ;;show paren
 
-;;language
-(global-company-mode 1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-linum-mode 1)
+(global-auto-revert-mode t)
+(abbrev-mode t)
+(delete-selection-mode t)
+(global-hl-line-mode t)
 
 ;;custom-variables
 (custom-set-variables
---
+++ lisp/init-packages.el
@@ -0,0 +1,70 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  ;;(setq package-archives '(("gnu" . "https://elpa.emacs-china.org/gnu/")
+  ;;			   ("melpa" . "https://elpa.emacs-china.org/melpa/"))))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/")))
+(require 'cl)
+;;add package here
+(defvar my/pkgs '(
+		 company
+		 solarized-theme
+		 hungry-delete
+		 smex
+		 smartparens
+		 js2-mode
+		 nodejs-repl
+		 exec-path-from-shell
+		 window-numbering
+		 paredit
+		 popwin
+		 ) "Default packages")
+(setq packages-selected-packages 'my/pkgs)
+
+(load-theme 'solarized-light t)
+
+(defun my/pkg-installed-p()
+  (loop for pkg in my/pkgs
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+(unless (my/pkg-installed-p)
+  (message "%s" "Refreshing packages database...")
+  (package-refresh-contents)
+  (dolist (pkg my/pkgs)
+     (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;;enable emacs to exec the shell commands
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(require 'hungry-delete)
+(global-hungry-delete-mode t)
+
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+
+(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+(require 'window-numbering)
+(window-numbering-mode t)
+
+;;config js2-mode for js files
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+(require 'popwin)
+(popwin-mode t)
+
+(require 'paredit)
+(add-hook 'scheme-mode-hook (lambda () (paredit-mode 1)))
+
+(global-company-mode 1)
+
+
+(provide 'init-packages)
